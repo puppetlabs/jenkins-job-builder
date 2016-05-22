f65fb1aae6bedc3b329180d44f1eaec89beca7d3
Wayne Warren <waynr+launchpad@sdf.org>
2016-05-22 15:04:10 -0700

    Rename Builder.delete_job to Builder.delete_jobs.
    Change-Id: If57a98a00e9577fdf7a3d7585905a2102d8c61cd

--- jenkins_jobs/builder.py
+++ jenkins_jobs/builder.py
@@ -259,7 +259,7 @@ class Builder(object):
                 logger.debug("Keeping job %s", job['name'])
         return deleted_jobs
 
-    def delete_job(self, jobs):
+    def delete_jobs(self, jobs):
         if jobs is not None:
             logger.info("Removing jenkins job(s): %s" % ", ".join(jobs))
         for job in jobs:
