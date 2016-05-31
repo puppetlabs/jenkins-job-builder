6e96af1b7a3a31e150549bae89796ee02c3e9ba6
Wayne Warren <waynr+launchpad@sdf.org>
2016-05-31 07:27:11 -0700

    Move macro expansion into YamlParser.
    Introduce the registry.MacroRegistry class to handle:
     * registration of macro types via setuptools' entrypoints
     * registration of individual macros for lookup by component list type
     * expansion of macros references during YAML "parsing"
    
    Probably the most important and objectionable consequence of this move
    is a decrease in macro expansion efficiency--rather than conveniently
    co-locating macro expansion in-line with xml expansion, this
    introduces a significant number of additional passes over job,
    job-template, defaults, and macro component lists. This decrease in
    efficiency can be observed as a roughly 30-50% increase in test time
    and will probably be nontrivial for JJB users with large amounts of
    YAML.
    
    I think that the separation of concerns here where the
    YamlPaser.expandYaml method produces a fully-expanded set of yaml
    data, is worth the short-term decrease in macro expansion efficiency.
    In the long run, we can consider alternative approaches to macro
    expansion efficiency such as restructuring the overall yaml expansion
    algorithm to make it suitable for parallelization (eg, expanding
    projects and/or job-groups concurrently or even in parallel if we are
    willing to trade-off memory effiency by spinning up subprocesses).
    
    It's also important to note that in the long run we really should move
    away from this almost purely-procedural approach to YAML expansion and
    consider something more object-oriented, in which case we need macro
    expansion to occur within the YamlParser anyway in order to more fully
    benefit from the object-oriented approach. For example, we might come
    to see macros as special cases of job-templates since they actually
    have similar data structures and use cases; this would allow the two
    to benefit to some extent from OO inheritance.
    
    Change-Id: I292c6b1f8472370282205426cd8ceb847eb969bd

--- jenkins_jobs/parser.py
+++ jenkins_jobs/parser.py
@@ -25,6 +25,7 @@ import os
 from jenkins_jobs.constants import MAGIC_MANAGE_STRING
 from jenkins_jobs.errors import JenkinsJobsException
 from jenkins_jobs.formatter import deep_format
+from jenkins_jobs.registry import MacroRegistry
 import jenkins_jobs.local_yaml as local_yaml
 from jenkins_jobs import utils
 
@@ -75,6 +76,8 @@ class YamlParser(object):
         self.jjb_config = jjb_config
         self.keep_desc = jjb_config.yamlparser['keep_descriptions']
         self.path = jjb_config.yamlparser['include_path']
+
+        self.__macro_registry = MacroRegistry()
 
     def load_files(self, fn):
 
@@ -213,6 +216,11 @@ class YamlParser(object):
             job["description"] = description + \
                 self._get_managed_string().lstrip()
 
+    def __register_macros(self):
+        for component_type in self.__macro_registry.component_types:
+            for macro in self.data.get(component_type, {}).values():
+                self.__macro_registry.register(component_type, macro)
+
     def expandYaml(self, registry, jobs_glob=None):
         changed = True
         while changed:
@@ -222,7 +230,11 @@ class YamlParser(object):
                     if module.handle_data(self.data):
                         changed = True
 
+        self.__register_macros()
+        for default in self.data.get('defaults', {}).values():
+            self.__macro_registry.expand_macros(default)
         for job in self.data.get('job', {}).values():
+            self.__macro_registry.expand_macros(job)
             if jobs_glob and not matches(job['name'], jobs_glob):
                 logger.debug("Ignoring job {0}".format(job['name']))
                 continue
@@ -354,6 +366,7 @@ class YamlParser(object):
                 template, params,
                 self.jjb_config.yamlparser['allow_empty_variables'])
 
+            self.__macro_registry.expand_macros(expanded, params)
             job_name = expanded.get('name')
             if jobs_glob and not matches(job_name, jobs_glob):
                 continue
